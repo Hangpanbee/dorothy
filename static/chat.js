d8676c29c8c8b2fa7125d1812535b961ad8b41a7
Rohit Gangupantulu <rbg5181@gmail.com>
2019-09-15 08:19:40 -0400

    Final push

--- static/chat.js
+++ static/chat.js
@@ -1,77 +1,81 @@
 var me = {};
-me.avatar = "Chatbotassets/avatar.png";
+me.avatar = "../static/avatar.png";
 
 var you = {};
-you.avatar = "Chatbotassets/therapist.png";
+you.avatar = "../static/therapist.png";
 
-function insert_chat(who,text){
-  var control = ''
-  if (who == 'me'){
-    control = '<li class="message right appeared">'+
-                  '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /> </div>'+
-                  '<div class="text_wrapper">'+
-                  '<div class="text">'+text+'</div>'+
-                  '</div>'+
-                  '</li>'
+function insert_chat(who, text) {
+  var control = "";
+  if (who == "me") {
+    control =
+      '<li class="message right appeared">' +
+      '<div class="avatar"><img class="img-circle" style="width:100%;" src="' +
+      me.avatar +
+      '" /> </div>' +
+      '<div class="text_wrapper">' +
+      '<div class="text">' +
+      text +
+      "</div>" +
+      "</div>" +
+      "</li>";
+  } else {
+    control =
+      '<li class="message left appeared">' +
+      '<div class="avatar"><img class="img-circle" style="width:100%;" src="' +
+      you.avatar +
+      '" /> </div>' +
+      '<div class="text_wrapper">' +
+      '<div class="text">' +
+      text +
+      "</div>" +
+      "</div>" +
+      "</li>";
   }
-  else{
-    control = '<li class="message left appeared">'+
-                  '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ you.avatar +'" /> </div>'+
-                  '<div class="text_wrapper">'+
-                  '<div class="text">'+text+'</div>'+
-                  '</div>'+
-                  '</li>';
-  }
-  setTimeout(
-        function(){                        
-            $("#ul_input").append(control).scrollTop($("#ul_input").prop('scrollHeight'));
-        });
-}
-
-
-function interact(message){
-console.log("STARTING");
-$.ajax({
-  type: 'POST',
-  url: '/test',
-  contentType: 'application/json',
-  dataType: 'json',
-  data:message,
- success: function(data){
-  insert_chat("you",data['text'])
- }
+  setTimeout(function() {
+    $("#ul_input")
+      .append(control)
+      .scrollTop($("#ul_input").prop("scrollHeight"));
   });
 }
 
-
-function get_message(){
-var message = document.getElementById("text_message").value;
-var json_data = {"msg":message}
-var sender = JSON.stringify(json_data)
-console.log(sender)
-console.log(message);
-insert_chat('me',message);
-interact(sender);
- }
-
-
-function get_audio(){
+function interact(message) {
+  console.log("STARTING");
   $.ajax({
-    type: 'POST',
+    type: "POST",
+    url: "/senti",
+    contentType: "application/json; charset = utf-8",
+    dataType: "json",
+    data: message,
+    success: function(data) {
+      insert_chat("you", data["text"]);
+    }
+  });
+}
+
+function get_message() {
+  var message = document.getElementById("text_message").value;
+  var json_data = { msg: message };
+  var sender = JSON.stringify(json_data);
+  console.log(sender);
+  console.log(message);
+  insert_chat("me", message);
+  interact(sender);
+}
+
+function get_audio() {
+  $.ajax({
+    type: "POST",
     url: "/audio",
-    context: 'application/json',
-    dataType: 'json',
-  }).done(function(){
-    insert_chat('me', message);
+    context: "application/json",
+    dataType: "json"
+  }).done(function() {
+    insert_chat("me", message);
     interact(sender);
   });
-
 }
 
 function startDictation() {
-
-  if (window.hasOwnProperty('webkitSpeechRecognition')) {
-
+  if (window.hasOwnProperty("webkitSpeechRecognition")) {
     var recognition = new webkitSpeechRecognition();
 
     recognition.continuous = false;
@@ -81,15 +85,14 @@ function startDictation() {
     recognition.start();
 
     recognition.onresult = function(e) {
-      document.getElementById('text_message').value
-                               = e.results[0][0].transcript;
+      document.getElementById("text_message").value =
+        e.results[0][0].transcript;
       recognition.stop();
-      document.getElementById('text_message').submit();
+      document.getElementById("text_message").submit();
     };
 
     recognition.onerror = function(e) {
       recognition.stop();
-    }
-
+    };
   }
 }
---
+++ static/sentimental.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+from flask import Flask, render_template, request, jsonify
+
+
+def sentimental_analysis(message):
+    a = TextBlob(message).sentiment.polarity
+    b = TextBlob(message).sentiment.subjectivity
+    return a, b
+
+
+print(sentimental_analysis('I feel really really sad'))
